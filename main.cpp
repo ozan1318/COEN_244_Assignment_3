909d31f3bbf35e24d8fba3e582cfe382c7a9bf60
ozana1318 <ozan.alptekin2002@gmail.com>
2021-11-24 15:12:21 -0500

    hello

--- main.cpp
+++ main.cpp
@@ -2,20 +2,27 @@
 #include "TA.h"
 #include <fstream>
 #include <string>
+#include <vector>
+
 
 int main() {
     std::string TAinput;
     int lineNum;
+    std::vector<std::string> fileHold;
 
-    std::ifstream TAin;
+    std::ifstream TAin("TA_File.txt");
+    std::ofstream TAwrite("TA_File.txt", std::ofstream::trunc);
     TAin.open("TA_File.txt");
     std::getline(TAin, TAinput);
     lineNum = std::stoi(TAinput);
     for (int i = 0; i < lineNum; i++){
-
+        std::getline(TAin, TAinput);
+        if (!TAinput.find("Alum")){
+            fileHold.push_back(TAinput);
+        }
     }
-
-
+    TAin.close();
+    TAwrite.open("TA_File.txt");
 
 
     return 0;
