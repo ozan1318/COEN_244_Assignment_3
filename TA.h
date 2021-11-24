27ed6b5b5478d230f041808f314cb0bf125db42a
ozana1318 <ozan.alptekin2002@gmail.com>
2021-11-24 14:58:11 -0500

    hello

---
+++ TA.h
@@ -0,0 +1,10 @@
+#ifndef UNTITLED1_TA_H
+#define UNTITLED1_TA_H
+
+
+class TA {
+
+};
+
+
+#endif //UNTITLED1_TA_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "TA.h"
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string TAinput;
+    int lineNum;
+
+    std::ifstream TAin;
+    TAin.open("TA_File.txt");
+    std::getline(TAin, TAinput);
+    lineNum = std::stoi(TAinput);
+    for (int i = 0; i < lineNum; i++){
+
+    }
+
+
+
+
     return 0;
 }
