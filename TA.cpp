f04335d32fa353c95e59a3c6f73f340fd6e59f40
ozan1318 <ozan.alptekin2002@gmail.com>
2021-11-24 22:28:52 -0500

    hallo

--- TA.cpp
+++ TA.cpp
@@ -1,1 +1,33 @@
 #include "TA.h"
+
+int TA::getStudentID() {
+    return Student_ID;
+}
+
+void TA::setStudentID(int input){
+    Student_ID = input;
+}
+
+std::string TA::getDept(){
+    return Dept;
+}
+
+void TA::setDept(std::string input){
+    Dept = input;
+}
+
+std::string TA::getStatus(){
+    return Status;
+}
+
+void TA::setStatus(std::string input){
+    Status = input;
+}
+
+int TA::getWorking_Hours(){
+    return Working_Hours;
+}
+
+void TA::setWorking_Hours(int input){
+    Working_Hours = input;
+}
--- TA.h
+++ TA.h
@@ -1,9 +1,22 @@
 #ifndef UNTITLED1_TA_H
 #define UNTITLED1_TA_H
-
+#include <string>
 
 class TA {
-
+private:
+    int Student_ID;
+    std::string Dept;
+    std::string Status;
+    int Working_Hours;
+public:
+    int getStudentID();
+    void setStudentID(int input);
+    std::string getDept();
+    void setDept(std::string input);
+    std::string getStatus();
+    void setStatus(std::string input);
+    int getWorking_Hours();
+    void setWorking_Hours(int input);
 };
 
 
